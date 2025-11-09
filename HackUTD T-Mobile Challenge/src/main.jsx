7c07843956dacb90becd73159f5bfe47ff0b998b
JMcCM <john.maccallum101@gmail.com>
2025-11-09 11:36:34 -0600

    Add react-router-dom version comment in main.jsx
    $ npm install react-router-dom@6.27.0
    to install react-router-dom

--- HackUTD T-Mobile Challenge/src/main.jsx
+++ HackUTD T-Mobile Challenge/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom" // npm install react-router-dom@6.27.0
 import App from "./App.jsx"
 import Survey from "./pages/Survey.jsx"
 import "./index.css"
